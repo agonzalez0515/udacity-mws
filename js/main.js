e8d1c6bd9cbbde556bddb5b3f2814db5b49d0d84
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-06-10 22:32:41 -0700

    Fix: Add alt text to json and html

--- js/main.js
+++ js/main.js
@@ -141,7 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = " "
+  image.alt = restaurant.photo_text
   li.append(image);
 
   const name = document.createElement('h1');
