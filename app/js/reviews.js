3b9a0181f67f887262e41e542a6bbffa837e7a3e
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-09-19 18:12:06 -0700

    Feat: Create review object to store in idb.

--- app/js/reviews.js
+++ app/js/reviews.js
@@ -1,36 +1,24 @@
 
-// listen on submit button
-// get the form data
-// store the form data in idb if offline
-// pass the data back to server to update database
-
 const form = document.getElementById("reviews-form");
   
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   let formData = new FormData(document.getElementById("reviews-form"));
-  // const dataObj = {};
-  
   formData = formData.entries();              
   let obj = formData.next();
   let retrieved = {};             
-  while(undefined !== obj.value) {    
+  while(obj.value !== undefined) {    
       retrieved[obj.value[0]] = obj.value[1];
       obj = formData.next();
   }
-  console.log('retrieved: ',retrieved);
+  
   let restID = window.location.href.split('?')[1].split('=')[1];
   let reviewObj = {};
+  reviewObj.id = Date.now();
   reviewObj.restaurant_id = parseInt(restID);
   reviewObj.name = retrieved.name;
   reviewObj.rating = parseInt(retrieved.rating);
   reviewObj.comments = retrieved.review;
-  // reviewObj.createdAt = Date.now();
-  // console.log(reviewObj)
-  DBHelper.saveNewReview(reviewObj);
   
-  location.reload(true);
+  DBHelper.saveNewReview(reviewObj);
 });
-
-
-
